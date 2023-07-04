909465113212712b8f2899f626e7459b8b75204f
Trevor DePew <t.depewmusic@gmail.com>
2023-07-03 20:26:05 -0400

    serverside setup of config files and initial db

--- server/base.py
+++ server/base.py
@@ -1,2 +1,14 @@
-from core import app
+# from core import app
+from flask import Flask
+
+api = Flask(__name__)
+
+@api.route('/profile')
+def my_profile():
+    response_body = {
+        "name": "Jorb",
+        "lastname": "Jorberson"
+    }
+
+    return response_body
 
