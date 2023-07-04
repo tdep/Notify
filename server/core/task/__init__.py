b01590fe6cdf76407bd0ac4596fe345f39b8d078
Trevor DePew <t.depewmusic@gmail.com>
2023-07-04 01:04:17 -0400

    environment setup

--- server/core/task/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-task = Blueprint('task', __name__, template_folder='templates')
-
-from . import views
--- server/core/views.py
+++
@@ -1,5 +0,0 @@
-from core import app
-
-@app.route('/')
-def index():
-    return 'Hello Boy-o!'
